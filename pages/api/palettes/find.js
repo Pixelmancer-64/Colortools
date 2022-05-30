a0dc478cd3c3927b9809c5e7d3c3fc204c01d7cf
Sotiris64 <hugobillemartins@gmail.com>
2022-05-30 20:17:48 -0300

    hi

--- pages/api/palettes/find.js
+++ pages/api/palettes/find.js
@@ -5,5 +5,6 @@ export default async function handler(req, res) {
   await openConnection();
 
   const response = await Palette.find();
+  console.log(response)
   res.status(200).json(response);
 }
