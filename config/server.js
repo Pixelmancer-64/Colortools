6efd59a2e628b34e7081e5b4fb97efa22c669c6e
Sotiris64 <hugobillemartins@gmail.com>
2022-05-30 20:36:51 -0300

    vai por favor

--- config/server.js
+++ config/server.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://colortools.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'http://colortools.vercel.app';
