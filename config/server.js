6a118eef5dba0a31d98a2bb5a3cadf3f9dfc4d37
Sotiris64 <hugobillemartins@gmail.com>
2022-05-30 20:11:53 -0300

    hi

--- config/server.js
+++ config/server.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://colortools-kphx1f6qt-sotiris64.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'colortools.vercel.app';
